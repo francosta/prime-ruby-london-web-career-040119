69ca11436b218df6ef8b932b4fe6768e8d75ae1b
Francisco Costa <francisco@fcosta.pt>
2019-03-20 10:07:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -15,3 +15,16 @@ require 'pry'
      true
    end
  end
+
+def prime?(n)
+  if n < 0 or n == 0 or n == 1
+    false
+  else
+    for d in 2..(n - 1)
+      if n % d == 0
+        false
+      end
+    end
+  end
+  true
+end
