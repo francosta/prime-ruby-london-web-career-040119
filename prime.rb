1b88a94c7066bf238c4e331e9801145042e7287b
Francisco Costa <francisco@fcosta.pt>
2019-03-20 10:07:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,20 +1,7 @@
 require 'pry'
 
 
- def prime?(integer)
-   array = (3..integer - 1).to_a
-
-   if integer <= 1
-     false
-   elsif integer <= 3
-     true
-   elsif integer % 2 == 0
-     false
-   else
-     array.each {|element| if integer % element == 0; return false end}
-     true
-   end
- end
+ 
 
 def prime?(n)
   if n < 0 or n == 0 or n == 1
