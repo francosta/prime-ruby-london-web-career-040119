51fe2ce451c2f03fe16e6f26d54980c401a8b1a6
Francisco Costa <francisco@fcosta.pt>
2019-03-20 10:11:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,18 @@
 require 'pry'
 
 
+ def prime?(integer)
+   array = (3..integer - 1).to_a
 
+   if integer <= 1
+     false
+   elsif integer <= 3
+     true
+   elsif integer % 2 == 0
+     false
+   else
+     array.each {|element| if integer % element == 0; return false end}
+     true
+   end
+ end
 
-def prime?(n)
-  if n <= 0
-    binding.pry
-    false
-  else
-    for d in 2..(n - 1)
-      if n % d == 0
-        false
-      end
-    end
-  end
-  true
-end
